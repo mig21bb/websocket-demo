17f7bf50b0d5ac7f9290214179be01329bdba261
miguelangel.bunuales <miguelangel.bunuales@ORD001971.masterd.com>
2018-11-22 17:34:10 +0100

    @CrossOrigin
    Anotación necesaria para poder obtener respuestas de la Api REST desde llamadas de AJAX mediante javascript
    En los repositorios de ciudades y estadosCielos.

---
+++ src/main/java/com/example/websocketdemo/controller/ChatController.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.websocketdemo.controller;
+
+/**
+ *
+ * @author miguelangel.bunuales
+ */
+import com.example.websocketdemo.model.ChatMessage;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatController {
+
+    @MessageMapping("/chat.sendMessage")
+    @SendTo("/topic/public")
+    public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
+        return chatMessage;
+    }
+    
+    @MessageMapping("/chat.addUser")
+    @SendTo("/topic/public")
+    public ChatMessage addUser(@Payload ChatMessage chatMessage, 
+                               SimpMessageHeaderAccessor headerAccessor) {
+        // Add username in web socket session
+        headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+        return chatMessage;
+    }
+
+    @MessageMapping("/chat.addUser")
+    @SendTo("/topic/{conversationId}")
+    public ChatMessage addUserInConversation(@Payload ChatMessage chatMessage, 
+                            @DestinationVariable String conversationId,
+                               SimpMessageHeaderAccessor headerAccessor) {
+        // Add username in web socket session
+        headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+        return chatMessage;
+    }
+    
+    @MessageMapping("/chat.sendMessage/{conversationId}")
+    @SendTo("/topic/{conversationId}")
+    public ChatMessage sendMessageToConvesationId(@Payload ChatMessage chatMessage,
+            @DestinationVariable String conversationId) {
+        return chatMessage;
+    }
+    
+
+}
